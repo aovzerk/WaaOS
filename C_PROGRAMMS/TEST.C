98f9d06492b0a54ed9e75211efeb2a521408685f
aovzerk <pikseloooooo@gmail.com>
2022-05-19 23:06:20 +0300

    bug fix

--- C_PROGRAMMS/TEST.C
+++ C_PROGRAMMS/TEST.C
@@ -6,7 +6,7 @@ void main(void)
 {   
     u_char16 my_text[512];
     u_char16 file_name[] = "txt";
-    daps my_daps = get_daps_file(file_name, (u_int16) my_text);
+    daps my_daps = get_r_daps_file(file_name, (u_int16) my_text);
     load_daps(&my_daps);
     print(my_text, White);
 }
