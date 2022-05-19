40104d00484757136c3e04609f45640877f5b730
aovzerk <pikseloooooo@gmail.com>
2022-05-19 17:13:18 +0400

    fix

--- C_PROGRAMMS/CALC.C
+++ C_PROGRAMMS/CALC.C
@@ -13,8 +13,7 @@ void main(void)
     u_long_int my_num1 = convert_to_number(my_str);
     u_long_int my_num2 = convert_to_number(my_str2);
     my_num1 = my_num1 + my_num2;
-    f_string my_str3 = clear_str(); 
-    my_str3 = covert_to_string(my_num1);
+    f_string my_str3 = covert_to_string(my_num1);
     print((u_char16 *)"Result: ", White);
     print(my_str3.data, White);
 }
