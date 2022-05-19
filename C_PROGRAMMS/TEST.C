62548b993f8f4fbcdbdc4861aca741697549e9e8
aovzerk <pikseloooooo@gmail.com>
2022-05-19 23:04:10 +0300

    Добавлен дисковый вывод

--- C_PROGRAMMS/TEST.C
+++ C_PROGRAMMS/TEST.C
@@ -3,10 +3,10 @@
 #include "BASE_LIB.H"
 
 void main(void)
-{
+{   
     u_char16 my_text[512];
-    daps my_daps = create_daps(1, (u_int16) my_text, 27);
-    u_char16 st = load_daps(&my_daps);
-    f_string mt_str = covert_to_string(st);
-    print_nl(my_text, White);
+    u_char16 file_name[] = "txt";
+    daps my_daps = get_daps_file(file_name, (u_int16) my_text);
+    load_daps(&my_daps);
+    print(my_text, White);
 }
