f0fb2022b42eeddbe0c9a02d3a8f2018cbab69b4
aovzerk <pikseloooooo@gmail.com>
2022-05-19 17:29:36 +0400

    добавлен выбор операций в калькуляторе

--- C_PROGRAMMS/CALC.C
+++ C_PROGRAMMS/CALC.C
@@ -10,9 +10,30 @@ void main(void)
     print((u_char16 *) "Input number2:> ", White);
     f_string my_str2 = input();
     print(new_line, Black);
+    print((u_char16 *) "Input operand:> ", White);
+    f_string my_op = input();
+    print(new_line, Black);
     u_long_int my_num1 = convert_to_number(my_str);
     u_long_int my_num2 = convert_to_number(my_str2);
-    my_num1 = my_num1 + my_num2;
+    
+    switch (my_op.data[0])
+    {
+        case '+':
+            my_num1 = my_num1 + my_num2;
+            break;
+        case '-':
+            my_num1 = my_num1 - my_num2;
+            break;
+        case '*':
+            my_num1 = my_num1 * my_num2;
+            break;
+        case '/':
+            my_num1 = my_num1 / my_num2;
+            break;
+        default:
+            break;
+    }
+    
     f_string my_str3 = covert_to_string(my_num1);
     print((u_char16 *)"Result: ", White);
     print(my_str3.data, White);
