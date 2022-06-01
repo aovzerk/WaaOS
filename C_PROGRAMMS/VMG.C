500ed0ceaa0b1d85a4e94dda2567acd249aa8a56
lllzebralll <alexei.zebra@yandex.ru>
2022-06-01 18:10:59 +0300

    VMG_update

--- C_PROGRAMMS/VMG.C
+++ C_PROGRAMMS/VMG.C
@@ -1,3 +1,11 @@
 #include "LIB/BASE_LIB.H"
+#include "LIB/GRAPHICS/GRAPHICS.H"
 void main(void){
+    // for (u_char8 i = 0; i < 255; i++)
+    // {
+        
+    // }
+    u_char8 mode = 0;
+    mode = get_video_mode();
+    print(convert_to_string((u_long_int64)mode).data, White);
 }
