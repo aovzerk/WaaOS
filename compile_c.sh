47fa22420dff0e60e798f367437ba352989c0083
aovzerk <pikseloooooo@gmail.com>
2024-12-06 16:11:52 +0400

    fix opt

--- compile_c.sh
+++ compile_c.sh
@@ -1,8 +1,8 @@
-gcc-11 -std=gnu99 -fpermissive -Os -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/HELLO_C.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/HELLO_C.C
-gcc-11 -std=gnu99 -fpermissive -Os -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/LS.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/LS.C
-gcc-11 -std=gnu99 -fpermissive -Os -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/CALC.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/CALC.C
-gcc-11 -std=gnu99 -fpermissive -Os -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/F_INFO.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/F_INFO.C
-gcc-11 -std=gnu99 -fpermissive -Os -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/GUI.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/GUI.C
-gcc-11 -std=gnu99 -fpermissive -Os -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/CMD.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/CMD.C
-gcc-11 -std=gnu99 -fpermissive -Os -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/POWEROFF.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/POWEROFF.C
-gcc-11 -std=gnu99 -fpermissive -Os -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/DRAW_BMP.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/DRAW_BMP.C
+gcc-11 -std=gnu99 -fpermissive -O0 -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/HELLO_C.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/HELLO_C.C
+gcc-11 -std=gnu99 -fpermissive -O0 -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/LS.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/LS.C
+gcc-11 -std=gnu99 -fpermissive -O0 -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/CALC.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/CALC.C
+gcc-11 -std=gnu99 -fpermissive -O0 -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/F_INFO.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/F_INFO.C
+gcc-11 -std=gnu99 -fpermissive -O0 -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/GUI.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/GUI.C
+gcc-11 -std=gnu99 -fpermissive -O0 -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/CMD.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/CMD.C
+gcc-11 -std=gnu99 -fpermissive -O0 -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/POWEROFF.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/POWEROFF.C
+gcc-11 -std=gnu99 -fpermissive -O0 -I KERNEL -nostdlib -m32 -march=i386 -fno-stack-protector -fno-pie -ffreestanding -o ./C_PROGRAMMS/DRAW_BMP.BIN -Wl,--nmagic,--script=linker.ld ./C_PROGRAMMS/DRAW_BMP.C
